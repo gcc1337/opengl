671b517fe32bb392b9299744147ea6b4dd4eb95a
gcc1337 <gustavo54535251@gmail.com>
2026-05-15 17:44:39 -0300

    missao 3 completa

--- main.cpp
+++ main.cpp
@@ -89,53 +89,47 @@ void keyboard(unsigned char key, int posX, int posY)
 
 void retangle()
 {
-    std::vector<std::vector<float>> pos{
-        //atras
-        {0.0f, 0.0f, 0.0f},
-        {1.0f, 0.0f, 0.0f},
-        {1.0f, 0.5f, 0.0f},
-        {0.0f, 0.5f, 0.0f},
+    std::vector<float> A{ -1.0f, 0.0f, 0.0f };
+    std::vector<float> B{ 0.0f, 1.0f, 0.0f };
+    std::vector<float> C{ 1.0f, 0.0f, 0.0f };
+    std::vector<float> D{ 0.5f, -1.0f, 0.0f };
+    std::vector<float> E{ -0.5f, -1.0f, 0.0f };
+    std::vector<float> picoPiramide{ 0.0f, 0.0f, 1.0f };
 
-        //frente
-        {0.0f, 0.0f, 1.0f},
-        {1.0f, 0.0f, 1.0f},
-        {1.0f, 0.5f, 1.0f},
-        {0.0f, 0.5f, 1.0f},
-
-        //esquerda
-        {0.0f, 0.0f, 0.0f},
-        {0.0f, 0.5f, 0.0f},
-        {0.0f, 0.5f, 1.0f},
-        {0.0f, 0.0f, 1.0f},
-
-        //direita
-        {1.0f, 0.0f, 0.0f},
-        {1.0f, 0.5f, 0.0f},
-        {1.0f, 0.5f, 1.0f},
-        {1.0f, 0.0f, 1.0f},
-
-
-        //baixo
-        {0.0f, 0.0f, 0.0f},
-        {1.0f, 0.0f, 0.0f},
-        {1.0f, 0.0f, 1.0f},
-        {0.0f, 0.0f, 1.0f},
-        
-        //cima
-        {0.0f, 0.5f, 0.0f},
-        {1.0f, 0.5f, 0.0f},
-        {1.0f, 0.5f, 1.0f},
-        {0.0f, 0.5f, 1.0f}
+    std::vector<std::vector<float>> posBase{
+        A,
+        B,
+        C,
+        D,
+        E
+    };
+    
+    std::vector<std::vector<std::vector<float>>> triangles{
+        {{A, B, picoPiramide},
+         {B, C, picoPiramide},
+         {C, D, picoPiramide},
+         {D, E, picoPiramide},
+         {E, A, picoPiramide}}
     };
 
     float cor[] = { 0.3, 0.6, 0.8 };
 
-    glBegin(GL_QUADS);
-        for (int i = 0; i < pos.size(); i++)
+
+    glBegin(GL_POLYGON);
+        for (int i = 0; i < posBase.size(); i++)
         {
-            glVertex3d(pos[i][0] - 0.5f, pos[i][1] - 0.25f, pos[i][2] - 0.5f);
+            glVertex3d(posBase[i][0], posBase[i][1], posBase[i][2]);
             glColor3f(cor[0]+ i * 0.01f, cor[1]+ i * 0.02f, cor[2]+ i * 0.03f);
         }
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+    for (auto& tri : triangles)
+    {
+        for(auto& vec: tri)
+            glVertex3d(vec[0], vec[1], vec[2]);
+            glColor3f(cor[0] += 0.1, cor[1] += 0.2, cor[2] += 0.3);
+    }
     glEnd();
 }
 
@@ -148,24 +142,24 @@ void draw()
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    //// desenhando o plano cartesiano 3D
-    //glBegin(GL_LINES);
-    //// eixo X
-    //glColor3f(0.0, 0.0, 0.0);
-    //glVertex3f(-2.0, 0.0, 0.0);
-    //glColor3f(1.0, 0.0, 0.0);
-    //glVertex3f(2.0, 0.0, 0.0);
-    //// eixo Y
-    //glColor3f(0.0, 0.0, 0.0);
-    //glVertex3f(0.0, -2.0, 0.0);
-    //glColor3f(0.0, 1.0, 0.0);
-    //glVertex3f(0.0, 2.0, 0.0);
-    //// eixo Z
-    //glColor3f(0.0, 0.0, 0.0);
-    //glVertex3f(0.0, 0.0, -2.0);
-    //glColor3f(0.0, 0.0, 1.0);
-    //glVertex3f(0.0, 0.0, 2.0);
-    //glEnd();
+    // desenhando o plano cartesiano 3D
+    glBegin(GL_LINES);
+    // eixo X
+    glColor3f(0.0, 0.0, 0.0);
+    glVertex3f(-2.0, 0.0, 0.0);
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f(2.0, 0.0, 0.0);
+    // eixo Y
+    glColor3f(0.0, 0.0, 0.0);
+    glVertex3f(0.0, -2.0, 0.0);
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(0.0, 2.0, 0.0);
+    // eixo Z
+    glColor3f(0.0, 0.0, 0.0);
+    glVertex3f(0.0, 0.0, -2.0);
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f(0.0, 0.0, 2.0);
+    glEnd();
 
     glLoadIdentity();
     confCamera();
