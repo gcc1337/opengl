246091dc0152944457ca81c6152dbb1abe9fc041
gcc1337 <gustavo54535251@gmail.com>
2026-05-29 13:28:50 -0300

    feito

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ float CamZ = 1.0;
 
 float deltaTime{};
 float lastFrame{};
-    
+
 // configura o campo de visualização
 void confCamera()
 {
@@ -42,26 +42,16 @@ void keyboard(unsigned char key, int posX, int posY)
     case 27:
         exit(0);
         break;
-    case 'w':
-        glTranslatef(0.0, 0.01, 0.0);
+    case 'x':
+        glScalef(-1.0f, 1.0f, 1.0f);
         break;
-    case 'a':
-        glTranslatef(-0.01, 0.0, 0.0);
-        break;
-
-    case 's':
-        glTranslatef(0.0, -0.01, 0.0);
-        break;
-
-    case 'd':
-        glTranslatef(0.01, 0.0, 0.0);
+    case 'y':
+        glScalef(1.0f, -1.0f, 1.0f);
         break;
     }
     // repinta a tela
     glutPostRedisplay();
 }
-
-
 
 // função de desenho
 void draw()
@@ -71,7 +61,7 @@ void draw()
     lastFrame = currentFrame;
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     //// desenhando o plano cartesiano 3D
     //glBegin(GL_LINES);
     //// eixo X
@@ -94,18 +84,50 @@ void draw()
     //glLoadIdentity();
     //confCamera();
 
-    glBegin(GL_TRIANGLES);
-        glColor3f(1.0f, 0.0f, 0.0f); // vermelho
-        glVertex2f(-0.5f, -0.5f);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glBegin(GL_QUADS);
+    glVertex2f(-0.6f, -0.5f);
+    glVertex2f(-0.4f, -0.5f);
+    glVertex2f(-0.4f, 0.2f);
+    glVertex2f(-0.6f, 0.2f);
 
-        glColor3f(0.0f, 1.0f, 0.0f); // verde
-        glVertex2f(0.5f, -0.5f);
-
-        glColor3f(0.0f, 0.0f, 1.0f); // azul
-        glVertex2f(0.0f, 0.5f);
     glEnd();
 
-    
+    glBegin(GL_QUADS);
+
+    glVertex2f(0.4f, -0.5f);
+    glVertex2f(0.6f, -0.5f);
+    glVertex2f(0.6f, 0.2f);
+    glVertex2f(0.4f, 0.2f);
+
+    glEnd();
+
+    glBegin(GL_QUADS);
+
+    glVertex2f(0.2f, -0.7f);
+    glVertex2f(0.6f, -0.7f);
+    glVertex2f(0.6f, -0.5f);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex2f(0.2f, -0.5f);
+    glColor3f(0.0f, 0.0f, 1.0f);
+
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+
+    glVertex2f(-0.4f, 0.2f);
+    glVertex2f(0.0f, 0.7f);
+    glVertex2f(0.0f, 0.2f);
+
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+
+    glVertex2f(0.0f, 0.2f);
+    glVertex2f(0.0f, 0.7f);
+    glVertex2f(0.4f, 0.2f);
+
+    glEnd();
 
     glFlush();
 }
